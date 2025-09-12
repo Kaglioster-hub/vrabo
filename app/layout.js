371596e45eea4f494c7f7e97d7e1787a2178b7f9
Kaglioster <davide.marzetti@gmail.com>
2025-09-12 04:02:35 +0200

    🐛 Fix: correct globals.css import path

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 // app/layout.js
 import "../styles/globals.css";
-import "../style../styles/globals.css";
+import "../styles/globals.css";
 import Script from "next/script";
 import ThemeProviders from "./ThemeProvider"; // <- corrisponde al tuo file app/ThemeProvider.jsx
 import ThemeToggle from "@/components/ThemeToggle";
